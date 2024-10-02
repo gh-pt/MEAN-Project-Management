d0df70fbc8dbe89af6f10bb311ea02167193871d
Priyanshu Tiwari <priyanshutiwari004@gmail.com>
2024-10-02 20:07:42 +0530

    Done

--- Server/dist/routes/Query.route.js
+++ Server/dist/routes/Query.route.js
@@ -6,12 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Query_controller_1 = require("../controllers/Query.controller");
 const router = express_1.default.Router();
-// CRUD routes for queries
+// Add the Query
 router.post("/add-query", Query_controller_1.addQuery);
+// Get all Queries
 router.get("/queries", Query_controller_1.getAllQueries);
+// Get Query by id
 router.get("/getQueryById/:id", Query_controller_1.getQueryById);
+// Update Query
 router.put("/updateQuery/:id", Query_controller_1.updateQuery);
+// Delete Query
 router.delete("/deleteQuery/:id", Query_controller_1.deleteQuery);
+// Reply to Query
 router.post("/replyToQuery/:id", Query_controller_1.addReplyToQuery);
+// Get all Queries by project
 router.get("/queriesByProject/:id", Query_controller_1.getQueriesByProject);
 exports.default = router;
